3c750d3fe32a839c357148647e8ae07fc336e249
Mark Henderson <markhend@gmail.com>
2020-05-29 15:46:07 -0500

    add error message to pycost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -7,9 +7,14 @@ def portfolio_cost(filename):
         cost = 0
         for line in f:
             line = line.split(',')
-            cost += int(line[1]) * float(line[2])
+            try:
+                cost += int(line[1]) * float(line[2])
+            except ValueError:
+                print("warning: missing data", line)
+                continue
         return cost
 
 
-cost = portfolio_cost('Data/portfolio.csv')
+cost = portfolio_cost('Data/missing.csv')
 print("Total cost", cost)
+
