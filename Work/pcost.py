de63dfde5e34a5363b0630ecf59db72cb5b296e1
Mark Henderson <markhend@gmail.com>
2020-05-29 15:03:29 -0500

    revise pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,10 @@
 # pcost.py
 #
 # Exercise 1.27
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    tc = 0
+    for line in f:
+        line = line.split(',')
+        tc += int(line[1]) * float(line[2])
+    print("Total cost", tc)
