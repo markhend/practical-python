ccf7de71cc3418030abfd861c3b157608722f8c3
Mark Henderson <markhend@gmail.com>
2020-05-29 12:17:20 -0500

    update

--- Work/sears.py
+++ Work/sears.py
@@ -1,6 +1,7 @@
 # sears.py
-bill_thickness = 0.11 * 0.001 # meters (0.11 mm)
-sears_height = 442 # meters
+
+bill_thickness = 0.11 * 0.001  # meters (0.11 mm)
+sears_height = 442  # meters
 num_bills = 1
 day = 1
 
@@ -12,4 +13,3 @@ while num_bills * bill_thickness < sears_height:
 print("Number of days", day)
 print("Number of bills", num_bills)
 print("Final height", num_bills * bill_thickness)
-
