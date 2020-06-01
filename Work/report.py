f6d24c8b70a72c4f48d5d76ea4dbb7473dfaad88
Mark Henderson <markhend@gmail.com>
2020-06-01 18:29:00 -0500

    update report.py

--- Work/report.py
+++ Work/report.py
@@ -8,28 +8,24 @@ def read_portfolio(filename):
     with open(filename) as f:
         rows = csv.reader(f)
         headers = next(rows)
-
         for row in rows:
             record = dict(zip(headers, row))
             stock = {
-                 'name'   : record['name'],
-                 'shares' : int(record['shares']),
-                 'price'   : float(record['price'])
+                'name': record['name'],
+                'shares': int(record['shares']),
+                'price': float(record['price'])
             }
             portfolio.append(stock)
-
     return portfolio
 
 
-# portfolio = read_portfolio('Data/portfolio.csv')
-portfolio = read_portfolio('Data/portfoliodate.csv')
-# pprint(portfolio)
-total_cost = 0.0
-for s in portfolio:
-    # print(s)
-    total_cost += s['shares'] * s['price']
-print(f"portfolio total cost basis {total_cost}")
-print()
+def portfolio_cost(portfolio):
+    # portfolio of stocks (dict of name, shares, price)
+    cost = 0.0
+    for s in portfolio:
+        cost += s['shares'] * s['price']
+    # print(f"portfolio total cost basis {cost}")
+    return cost
 
 
 def read_prices(filename):
@@ -46,14 +42,16 @@ def read_prices(filename):
     return(prices)
 
 
-current_prices = read_prices('Data/prices.csv')
+# prices = read_prices('Data/prices.csv')
 # pprint(current_prices)
 
-total_value = 0.0
-for holding in portfolio:
-    total_value += (current_prices[holding['name']] -
-                    holding['price']) * holding['shares']
-print(f"portolio gain/loss {total_value:0.2f}\n")
+def portfolio_value_change(portfolio, prices):
+    value = 0.0
+    for s in portfolio:
+        value += (prices[s['name']] -
+                  s['price']) * s['shares']
+    # print(f"portolio gain/loss {value:0.2f}\n")
+    return value
 
 
 def make_report(portfolio, prices):
@@ -65,12 +63,26 @@ def make_report(portfolio, prices):
     return report
 
 
-report = make_report(portfolio, current_prices)
-print('%10s %10s %10s %10s' % ('Name', 'Shares', 'Price', 'Change'))
-seps = ('-'*10)
-print('%10s %10s %10s %10s' % (seps, seps, seps, seps))
-for r in report:
-    print('%10s %10d %10s %10.2f' % (r[0], r[1], '$'+str(r[2]), r[3]))
-print()
-# for name, shares, price, change in report:
-#     print(f'{name:>10s} {shares:>10d} {price:>10.2f} {change:>10.2f}')
+def print_report(report):
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print('%10s %10s %10s %10s' % headers)
+    print(('-' * 10 + ' ') * len(headers))
+    for r in report:
+        print('%10s %10d %10s %10.2f' % (r[0], r[1], '$'+str(r[2]), r[3]))
+    # for name, shares, price, change in report:
+    #     print(f'{name:>10s} {shares:>10d} {price:>10.2f} {change:>10.2f}')
+
+
+def portfolio_report(portfolio_filename, prices_filename):
+    portfolio = read_portfolio(portfolio_filename)
+    prices = read_prices(prices_filename)
+    report = make_report(portfolio, prices)
+    print_report(report)
+
+
+# portfolio_report('Data/portfolio.csv', 'Data/prices.csv')
+files = ['Data/portfolio.csv', 'Data/portfolio2.csv']
+for name in files:
+    print(f'{name:-^43s}')
+    portfolio_report(name, 'Data/prices.csv')
+    print()
