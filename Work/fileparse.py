691fbd84397bd627c17ef72a9e289ce9c676b0a3
Mark Henderson <markhend@gmail.com>
2020-06-03 08:15:42 -0500

    clean files

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,44 +1,47 @@
+# fileparse.py
 import csv
 
-
-def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=',',
-              silence_errors=False):
+def parse_csv(lines, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
     '''
-    Parse a CSV file into a list of records
+    Parse a CSV file into a list of records with type conversion.
     '''
-    if select and has_headers == False:
-        raise RuntimeError("select argument requires columnn headers")
-    with open(filename) as f:
-        rows = csv.reader(f, delimiter=delimiter)
-        indices = []
+    if select and not has_headers:
+        raise RuntimeError('select requires column headers')
 
-        if has_headers:
-            headers = next(rows)
-            # If a column selector was given, find indices of the specified columns.
-            # Also narrow the set of headers used for resulting dictionaries
-            if select:
-                indices = [headers.index(colname) for colname in select]
-                headers = select
+    rows = csv.reader(lines, delimiter=delimiter)
 
-        records = []
-        for row_no, row in enumerate(rows):
+    # Read the file headers (if any)
+    headers = next(rows) if has_headers else []
+
+    # If specific columns have been selected, make indices for filtering and set output columns
+    if select:
+        indices = [ headers.index(colname) for colname in select ]
+        headers = select
+
+    records = []
+    for rowno, row in enumerate(rows, 1):
+        if not row:     # Skip rows with no data
+            continue
+
+        # If specific column indices are selected, pick them out
+        if select:
+            row = [ row[index] for index in indices]
+
+        # Apply type conversion to the row
+        if types:
             try:
-                if not row:    # Skip rows with no data
-                    continue
-                if types:
-                    row = [func(val) for func, val in zip(types, row)]
-                # Filter the row if specific columns were selected
-                if indices:
-                    row = [row[index] for index in indices]
-                if not has_headers:
-                    records.append(tuple(row))
-                else:
-                    # Make a dictionary
-                    record = dict(zip(headers, row))
-                    records.append(record)
+                row = [func(val) for func, val in zip(types, row)]
             except ValueError as e:
                 if not silence_errors:
-                    print(f"Row {row_no}: Couldn't convert {row}")
-                    print(f"Row {row_no}: Reason {e}")
+                    print(f"Row {rowno}: Couldn't convert {row}")
+                    print(f"Row {rowno}: Reason {e}")
+                continue
+
+        # Make a dictionary or a tuple
+        if headers:
+            record = dict(zip(headers, row))
+        else:
+            record = tuple(row)
+        records.append(record)
 
     return records
--- Work/report.py
+++ Work/report.py
@@ -1,99 +1,64 @@
-import csv
-import fileparse
+# report.py
 
+import fileparse
 
 def read_portfolio(filename):
     '''
     Read a stock portfolio file into a list of dictionaries with keys
     name, shares, and price.
     '''
-    # portfolio = []
-    # with open(filename) as f:
-    #     rows = csv.reader(f)
-    #     headers = next(rows)
-    #     for row in rows:
-    #         record = dict(zip(headers, row))
-    #         stock = {
-    #             'name': record['name'],
-    #             'shares': int(record['shares']),
-    #             'price': float(record['price'])
-    #         }
-    #         portfolio.append(stock)
-    return fileparse.parse_csv(filename, select=['name', 'shares', 'price'], types=[str, int, float])
-
-
-def portfolio_cost(portfolio):
-    # portfolio of stocks (dict of name, shares, price)
-    cost = 0.0
-    for s in portfolio:
-        cost += s['shares'] * s['price']
-    # print(f"portfolio total cost basis {cost}")
-    return cost
-
+    with open(filename) as lines:
+        return fileparse.parse_csv(lines, select=['name','shares','price'], types=[str,int,float])
 
 def read_prices(filename):
     '''
     Read a CSV file of price data into a dict mapping names to prices.
     '''
-    # prices = {}
-    # with open(filename, 'r') as f:
-    #     rows = csv.reader(f)
-    #     for row in rows:
-    #         try:
-    #             stock, price = row
-    #             prices[stock] = float(price)
-    #         except ValueError:
-    #             # print("missing data...skipping row")
-    #             continue
-    return dict(fileparse.parse_csv(filename, types=[str, float], has_headers=False))
+    with open(filename) as lines:
+        return dict(fileparse.parse_csv(lines, types=[str,float], has_headers=False))
 
+def make_report_data(portfolio,prices):
+    '''
+    Make a list of (name, shares, price, change) tuples given a portfolio list
+    and prices dictionary.
+    '''
+    rows = []
+    for stock in portfolio:
+        current_price = prices[stock['name']]
+        change = current_price - stock['price']
+        summary = (stock['name'], stock['shares'], current_price, change)
+        rows.append(summary)
+    return rows
 
-# prices = read_prices('Data/prices.csv')
-
-def portfolio_value_change(portfolio, prices):
-    value = 0.0
-    for s in portfolio:
-        value += (prices[s['name']] -
-                  s['price']) * s['shares']
-    # print(f"portolio gain/loss {value:0.2f}\n")
-    return value
-
-
-def make_report(portfolio, prices):
-    report = []
-    for s in portfolio:  # s = stock
-        price_change = prices[s['name']] - s['price']
-        report.append(
-            (s['name'], s['shares'], prices[s['name']], price_change))
-    return report
-
-
-def print_report(report):
-    headers = ('Name', 'Shares', 'Price', 'Change')
+def print_report(reportdata):
+    '''
+    Print a nicely formated table from a list of (name, shares, price, change) tuples.
+    '''
+    headers = ('Name','Shares','Price','Change')
     print('%10s %10s %10s %10s' % headers)
-    print(('-' * 10 + ' ') * len(headers))
-    for r in report:
-        print('%10s %10d %10s %10.2f' % (r[0], r[1], '$'+str(r[2]), r[3]))
-    # for name, shares, price, change in report:
-    #     print(f'{name:>10s} {shares:>10d} {price:>10.2f} {change:>10.2f}')
+    print(('-'*10 + ' ')*len(headers))
+    for row in reportdata:
+        print('%10s %10d %10.2f %10.2f' % row)
 
+def portfolio_report(portfoliofile, pricefile):        
+    '''
+    Make a stock report given portfolio and price data files.
+    '''
+    # Read data files 
+    portfolio = read_portfolio(portfoliofile)
+    prices = read_prices(pricefile)
 
-def portfolio_report(portfolio_filename, prices_filename):
-    portfolio = read_portfolio(portfolio_filename)
-    prices = read_prices(prices_filename)
-    report = make_report(portfolio, prices)
+    # Create the report data
+    report = make_report_data(portfolio, prices)
+
+    # Print it out
     print_report(report)
 
-
-def main(argv):
-    files = argv[1:-1]
-    for name in files:
-        print(f'{name:-^43s}')
-        portfolio_report(name, 'Data/prices.csv')
-        print()
-
+def main(args):
+    if len(args) != 3:
+        raise SystemExit('Usage: %s portfile pricefile' % args[0])
+    portfolio_report(args[1], args[2])
 
 if __name__ == '__main__':
-    # files = ['Data/portfolio.csv', 'Data/portfolio2.csv']
     import sys
     main(sys.argv)
